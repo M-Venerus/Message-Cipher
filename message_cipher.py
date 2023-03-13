5fe1d085e6f2d8ab39e2c4fc587d28121af432f3
M-Venerus <mikey.motorist@outlook.com>
2023-03-13 13:55:15 -0400

    Update cipher code to expand calculations inside def for readability

--- message_cipher.py
+++ message_cipher.py
@@ -1,33 +1,48 @@
 # Message Cipher
-# Will allow user to encrypt or decrypt a message by form or offsetting letters in the alphabet.
+# Will allow user to encrypt or decrypt a message by form or offsetting text in the alphabet.
 # User will select function to encrypt or decrypt, input the message, and input the offset amount.
 # Program will output encrypted or decrypted message and prompt user if they would like to use again.
 
-# TODO create new file with alphabet list and import file
+import alphabet_list
 
-# TODO create encrypt function
+def encrypt(text, shift):
+    generated_text = ""
+    for letter in text:
+        if letter in alphabet_list.alphabet:
+            letter_index = alphabet_list.alphabet.index(letter)
+            new_letter_index = letter_index + shift
+            new_letter = alphabet_list.alphabet[new_letter_index]
+            generated_text += new_letter
+        else: 
+            generated_text += letter
+    print(f"Here is the ecoded result:\n{generated_text}")
 
+def decrypt(text, shift):
+    generated_text = ""
+    for letter in text:
+        if letter in alphabet_list.alphabet:
+            letter_index = alphabet_list.alphabet.index(letter)
+            new_letter_index = letter_index - shift
+            new_letter = alphabet_list.alphabet[new_letter_index]
+            generated_text += new_letter
+        else: 
+            generated_text += letter
+    print(f"Here is the decoded result:\n{generated_text}")
 
+print("Welcome to Message Cipher.")
 
-# TODO create decrypt function
+# While loop added to keep program running if user wants after initial message generated
 
+keep_running = "yes"
+while keep_running == "yes":
 
+    encrypt_or_decrypt = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text_input = input("Type your message.\n").lower()
+    shift_input = int(input("Shift amount:\n"))
 
-# TODO prompt user for input to encrypt or decrypt
+    if encrypt_or_decrypt == "encode":
+        encrypt(text_input, shift_input)
+    else:
+        decrypt(text_input, shift_input)
 
-
-
-# TODO prompt user for input of message
-
-
-
-# TODO prompt user for input of letter shift amount
-
-
-
-# TODO call function based off user input and apply other user input data as arguments
-# Functions should print the result and let the user know if it was encoded or decoded.
-
-
-
-# TODO prompt user if they would like to run program again using while loop
+    keep_running = input("Type 'yes' to run again, otherwise type 'no':\n")
