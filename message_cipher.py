6996641f439983b1f7948f1f750f40d91d10f7ac
M-Venerus <mikey.motorist@outlook.com>
2023-03-13 12:02:58 -0400

    Create initial program file and break down TODO items in pseudocode

---
+++ message_cipher.py
@@ -0,0 +1,33 @@
+# Message Cipher
+# Will allow user to encrypt or decrypt a message by form or offsetting letters in the alphabet.
+# User will select function to encrypt or decrypt, input the message, and input the offset amount.
+# Program will output encrypted or decrypted message and prompt user if they would like to use again.
+
+# TODO create new file with alphabet list and import file
+
+# TODO create encrypt function
+
+
+
+# TODO create decrypt function
+
+
+
+# TODO prompt user for input to encrypt or decrypt
+
+
+
+# TODO prompt user for input of message
+
+
+
+# TODO prompt user for input of letter shift amount
+
+
+
+# TODO call function based off user input and apply other user input data as arguments
+# Functions should print the result and let the user know if it was encoded or decoded.
+
+
+
+# TODO prompt user if they would like to run program again using while loop
